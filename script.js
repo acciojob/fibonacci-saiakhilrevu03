5360036c1d49d915ca33ecbe45e1c785a8f90cfd
saiakhilrevu03 <102013188+saiakhilrevu03@users.noreply.github.com>
2025-07-30 15:31:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function fibonacci(num) {
 // your code here
+	if(num===0) return 0;
+	if(num===1) return 1;
+	let first=fibonacci(num-1);
+	let second = fibonacci(num-2);
+	return first+second;
 }
 
 module.exports = fibonacci;
